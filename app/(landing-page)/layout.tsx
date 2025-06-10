ddf89d66e912a62f6d3d4c39676cfe5763bb8404
GuilhermeAlvesTeixeira <guilhermealvesteixeirasilva@gmail.com>
2025-06-10 09:27:15 -0300

    Alterando o projeto para nextJs

---
+++ app/(landing-page)/layout.tsx
@@ -0,0 +1,23 @@
+import { Footer } from "./footer";
+import { Header } from "./header";
+
+
+type Props = {
+    children: React.ReactNode;
+
+}
+
+const LandingPage = ({children}: Props) => {
+    return (
+        <div className = "min-h-screen flex flex-col">
+            <Header />
+
+            <main className = "flex-1 flex flex-col items-center justify-center">
+                {children}
+            </main>
+            <Footer/>
+        </div>
+    )
+}
+
+export default LandingPage;
---
+++ app/(landing-page)/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+  return (
+
+    <div>
+      Essa é a landing Page
+    </div>
+  )
+}
