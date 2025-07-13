8a017ae6ecd5bcfa3c15b9b80b2d6d0e6ab06aa7
GuilhermeAlvesTeixeira <guilhermealvesteixeirasilva@gmail.com>
2025-07-13 01:59:52 -0300

    Instalando dependencias do fabric.js e criando pagina de laboratorio

---
+++ app/(main)/moluscos/laboratorio/page.tsx
@@ -0,0 +1,10 @@
+
+const LaboratorioPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1>Olá laboratóroi</h1>
+    </div>
+  );
+};
+
+export default LaboratorioPage;
